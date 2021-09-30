0d41d544fd2dc4f04845f9c99554408b93c670d0
fblanco1990 <fblanco1990@gmail.com>
2021-09-30 16:48:40 -0500

    como utlizar un post y un put con state

--- src/app/service/http-service.service.ts
+++ src/app/service/http-service.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
-
-  constructor(private httpClient: HttpClient) { }
+  private headers: HttpHeaders;
+  constructor(private httpClient: HttpClient) {
+    this.setHeader();
+  }
 
 
   public get<T> (url: string, params: HttpParams = new HttpParams()): Observable<T> {
     return this.httpClient.get<T>(url, {params}) ;
   }
 
-  public post<T> (url: string, params: HttpParams = new HttpParams()): Observable<T> {
-    return this.httpClient.post<T>(url, {params}) ;
+  // public post<T> (url: string,body: string, auth: boolean = true): Observable<T> {
+  //   return this.httpClient.post<T>(url,body, { headers: auth? this.headers}) ;
+  // }
+
+  public post<T> (url: string, body: string): Observable<T> {
+    return this.httpClient.post<T>(url,body, {headers: this.headers}) ;
   }
+
 
   public put<T> (url: string, params: HttpParams = new HttpParams()): Observable<T> {
     return this.httpClient.put<T>(url, {params}) ;
@@ -24,5 +31,12 @@ export class HttpServiceService {
 
   public delet<T> (url: string, params: HttpParams = new HttpParams()): Observable<T> {
     return this.httpClient.delete<T>(url, {params}) ;
+  }
+
+  private setHeader() {
+    this.headers = new HttpHeaders()
+    .set('Accept', 'application/json')
+    .set('Content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*')
   }
 }
